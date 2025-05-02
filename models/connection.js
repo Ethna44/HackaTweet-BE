bba14f7ef5167cbf733e08b60da8ebc4dcf968ef
Ethan <ethan.frappie@gmail.com>
2025-05-02 09:34:57 +0200

    users schema

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://Reazay:q0gjUs0GfVsUycyJ@cluster0.cl18hjd.mongodb.net/';
+const connectionString = 'mongodb+srv://Reazay:q0gjUs0GfVsUycyJ@cluster0.cl18hjd.mongodb.net/HackaTweet';
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))

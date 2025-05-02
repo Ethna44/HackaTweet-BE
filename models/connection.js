d65b26b21a8fee3c8d4199f5554ade4cf0317e18
Ethan <ethan.frappie@gmail.com>
2025-05-02 09:21:19 +0200

    add models

---
+++ models/connection.js
@@ -0,0 +1,8 @@
+
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://Reazay:q0gjUs0GfVsUycyJ@cluster0.cl18hjd.mongodb.net/';
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
